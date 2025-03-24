1499bfa434e7aa88f9e5435428abba55c89d1395
SaraLaouini1 <laouinisara4@gmail.com>
2025-03-24 16:12:43 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-#import { useNavigate } from 'react-router-dom'
 import Chat from './components/Chat'
 import Login from './components/Login'
 import { useEffect, useState } from 'react'
